b3936aa607cba5ff2242cec1bef396eac3bc97d3
LizGuile <lizguile19@gmail.com>
2023-02-07 23:43:26 +0800

    binarySearch

--- Design Pattern/Binary Search/Search_Binary.js
+++ Design Pattern/Binary Search/Search_Binary.js
@@ -1,7 +1,17 @@
 function binarySearch(arr,elem) {
 	var start = 0;
 	var end = arr.length - 1;
-	var middle = Math.floor((start + end) / 2);
-	
+	while (arr[middle] !== elem) {
+/* 		if (arr[middle] === elem){
+			console.log(middle)
+		}
+		else  */
+		if (elem < arr[middle]) {
+			end = middle - 1;
+		} else {
+			start = middle + 1;
+		}
+		middle = Math.floor((start + end) / 2);
+	}
 }
 binarySearch([2,5,6,9,13,15,28],15)
