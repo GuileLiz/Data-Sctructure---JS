f029ba98954380380957116e0419e6896d1ce9a9
LizGuile <lizguile19@gmail.com>
2023-02-08 00:02:00 +0800

    shotest binarySearch

--- Design Pattern/Binary Search/Search_Binary.js
+++ Design Pattern/Binary Search/Search_Binary.js
@@ -3,11 +3,8 @@ function binarySearch(arr,elem) {
 	var end = arr.length - 1;
 	var middle = Math.floor((start + end) / 2);
 	while (arr[middle] !== elem && start <= end) {
-		if (elem < arr[middle]) {
-			end = middle - 1;
-		} else {
-			start = middle + 1;
-		}
+		if (elem < arr[middle]) end = middle - 1;
+		else start = middle + 1;
 		middle = Math.floor((start + end) / 2);
 	}
 	return arr[middle] === elem ? middle:"Not in the array"
